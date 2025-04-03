9f1723c8086c72bb9399a3cb6bca79efbd6fbc8c
lichanglin <beelan@yeah.net>
2025-04-03 11:43:25 +0800

    test: :white_check_mark: test depend on vitest

---
+++ packages/core/src/text/__tests__/text.test.tsx
@@ -0,0 +1,5 @@
+import { describe, it } from 'vitest'
+
+describe('text.tsx', () => {
+	it.todo('should render text')
+})
--- packages/utils/src/usable.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @description 返回状态类名
- * @param name 状态名
- * @param state 状态值
- */
-export const usable = (name: string, state?: boolean, prefix = 'is-') => {
-	return state ? `${prefix}${name}` : ''
-}
