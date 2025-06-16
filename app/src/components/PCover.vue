eaf045d3313736e2e38e49af762eda2471af42f2
bee1an <beelan@yeah.net>
2025-06-16 17:40:27 +0800

    chore: :hammer: cover use unsplash

--- app/src/components/PCover.vue
+++ app/src/components/PCover.vue
@@ -1,5 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { createApi } from 'unsplash-js'
+import { useStorage } from '@vueuse/core'
+import { Button } from '@yyui/yy-ui'
+
+const url = useStorage('URL', '')
+
+const api = createApi({
+	accessKey: 'N96Xr_q3HeOJQ29LrANQkM_2EhZqWvmidNDq-0lf3Bk'
+})
+
+const getNew = () => {
+	api.photos.getRandom({ count: 1 }).then((result) => {
+		console.log('result', result)
+
+		if (result.errors) {
+			url.value = ''
+		} else {
+			url.value = (result.response as any[])[0].urls.regular
+		}
+	})
+}
+
+if (!url.value) getNew()
+</script>
 
 <template>
-	<div w-full h-200px border-b="1px solid amber"></div>
+	<div
+		w-full
+		h-300px
+		border-b="1px solid amber"
+		overflow-hidden
+		flex="~ col"
+		justify-center
+		items-center
+		pos-relative
+	>
+		<div pos-absolute pos-bottom-10px pos-right-9xl>
+			<Button type="primary" @click="getNew">get new</Button>
+		</div>
+
+		<img :src="url" w-full @dragstart.prevent />
+	</div>
 </template>
--- app/src/components/PPaperboard.vue
+++ app/src/components/PPaperboard.vue
@@ -3,10 +3,10 @@ import { PPage } from 'yak-paper'
 </script>
 
 <template>
-	<div h-screen flex flex-col class="wrapper">
+	<div class="wrapper">
 		<!-- <div class="h-screen flex flex-col border-1 border-cyan border-solid"> -->
 		<!-- <div class="h-200px flex items-center justify-center bg-cyan-1">banner</div> -->
-		<div flex-1><PPage></PPage></div>
+		<div h-xl><PPage></PPage></div>
 	</div>
 </template>
 
