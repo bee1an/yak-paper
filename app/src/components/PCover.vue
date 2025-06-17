30c957351240130b7b22776871670457e4b51145
bee1an <beelan@yeah.net>
2025-06-17 12:45:16 +0800

    chore: :hammer: unsplash params

--- app/src/components/PCover.vue
+++ app/src/components/PCover.vue
@@ -3,7 +3,7 @@ import { createApi } from 'unsplash-js'
 import { useStorage } from '@vueuse/core'
 import { Button } from '@yyui/yy-ui'
 
-const url = useStorage('URL', '')
+const store = useStorage('URL', { url: '', timestamp: +new Date() })
 
 const api = createApi({
 	accessKey: 'N96Xr_q3HeOJQ29LrANQkM_2EhZqWvmidNDq-0lf3Bk'
@@ -11,34 +11,47 @@ const api = createApi({
 
 const getNew = () => {
 	api.photos.getRandom({ count: 1 }).then((result) => {
-		console.log('result', result)
+		let url = '',
+			timestamp = +new Date()
 
 		if (result.errors) {
-			url.value = ''
+			url = ''
 		} else {
-			url.value = (result.response as any[])[0].urls.regular
+			const newUrl = new URL((result.response as any[])[0].urls.regular)
+
+			newUrl.searchParams.set('w', '1200')
+			newUrl.searchParams.set('h', '300')
+
+			url = newUrl.toString()
+		}
+
+		store.value = {
+			url,
+			timestamp
 		}
 	})
 }
 
-if (!url.value) getNew()
+if (!store.value.url || store.value.timestamp + (1000 * 60 * 60) / 50 < +new Date()) getNew()
+
+// if (!url.value) getNew()
 </script>
 
 <template>
 	<div
 		w-full
 		h-300px
-		border-b="1px solid amber"
 		overflow-hidden
 		flex="~ col"
 		justify-center
 		items-center
 		pos-relative
+		bg-gray-1
 	>
 		<div pos-absolute pos-bottom-10px pos-right-9xl>
 			<Button type="primary" @click="getNew">get new</Button>
 		</div>
 
-		<img :src="url" w-full @dragstart.prevent />
+		<img :src="store.url" w-full @dragstart.prevent />
 	</div>
 </template>
